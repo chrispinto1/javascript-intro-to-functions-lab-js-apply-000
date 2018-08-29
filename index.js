8c9a3a6e740b2afce6f293e36c212a2b2e2ce709
Christian Pinto <pintochristian2@gmail.com>
2018-08-29 02:54:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whiper(string){
+  return string.toLowerCase()
+}
+
+function logshout(string){
+  console.log(string.toUpperCase())
+}
+

bfbaa3abdfeac7cd80c0e4f9d404c33be2f08ec6
Christian Pinto <pintochristian2@gmail.com>
2018-08-29 02:55:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logshout(string){
-  console.log(string.toUpperCase());
+function logShout(string){
+  console.log(string.toUpperCase())
 }
 
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
